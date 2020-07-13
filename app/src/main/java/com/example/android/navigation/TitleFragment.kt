71a3bcdceceada8b5a5fc644b527e583ad359884
King-Adolphus <ikechukwuachom@gmail.com>
2020-07-13 15:31:06 +0000

    first commit

---
+++ app/src/main/java/com/example/android/navigation/TitleFragment.kt
@@ -0,0 +1,10 @@
+package com.example.android.navigation
+
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+
+class TitleFragment : AppCompatActivity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+    }
+}
